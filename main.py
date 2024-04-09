cc875bc90c51031924c33b9845d64807d0c60f67
bobbyhadz <opmftw@gmail.com>
2024-04-09 12:21:21 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Multiple lines user Input in Python
+
+lines = []
+
+while True:
+    user_input = input()
+
+    # 👇️ if user pressed Enter without a value, break out of loop
+    if user_input == '':
+        break
+    else:
+        lines.append(user_input + '\n')
+
+
+# 👇️ prints list of strings
+print(lines)
+
+# 👇️ join list into a string
+print(''.join(lines))
